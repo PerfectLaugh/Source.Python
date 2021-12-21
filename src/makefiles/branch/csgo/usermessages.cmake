77a94c0bbd17c008b7615fc13e709f9f20865f9e
PerfectLaugh <20559490+PerfectLaugh@users.noreply.github.com>
2021-12-21 14:28:16 +0800

    Delete original build system

--- src/makefiles/branch/csgo/usermessages.cmake
+++
@@ -1,8 +0,0 @@
-SET(SOURCEPYTHON_MESSAGES_MODULE_GAME_SOURCES
-    # As these are not in any LIB files for the SDK, I think this is the best way
-    # of compiling them for the time being until Allied modders puts them in a .lib
-    hl2sdk/csgo/public/game/shared/csgo/protobuf/cstrike15_usermessages.pb.cc
-    hl2sdk/csgo/public/game/shared/csgo/protobuf/cstrike15_usermessage_helpers.cpp
-    hl2sdk/csgo/public/engine/protobuf/netmessages.pb.cc    
-    #---------------------------------------------------------------------------
-)
